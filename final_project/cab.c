c4ca5d2080573c4afffede68a8ce10e620ab4790
Bernardo Falé <mbfale@ua.pt>
2022-12-05 18:47:13 +0000

    qwer

--- final_project/cab.c
+++ final_project/cab.c
@@ -1,5 +1,5 @@
 #include "cab.h"
-#define N_TASKS 2
+
 
 cab* open_cab(char* name, uint16_t num, uint16_t dim, uint16_t* first){
 
@@ -8,13 +8,11 @@ cab* open_cab(char* name, uint16_t num, uint16_t dim, uint16_t* first){
     buffer->dim = dim;
     buffer->size = num * dim;
     buffer->c = 0;
-    buffer->buffers
     /* The minimum number of buffers needed for a CAB to avoid conflicts 
     must be equal to the number of tasks which share the CAB plus one */
     buffer->buffers = (uint16_t *) malloc(buffer->size * N_TASKS); 
     buffer->name = (char *) malloc(strlen(name));
     buffer->head = (uint16_t *) malloc(buffer->size);
-
     strcpy(buffer->name, name);
     buffer->head = first;
     return buffer;
@@ -22,13 +20,22 @@ cab* open_cab(char* name, uint16_t num, uint16_t dim, uint16_t* first){
 
 uint16_t* reserve(cab* cab_id){
     uint16_t* buffer;
-    buffer = cab_id->buffers;
+    for(int i = 0; i< N_TASKS; i++){
+        if(cab_id->flags[i] == 0){
+            buffer = cab_id->buffers + (i * cab_id->num);
+            cab_id->flags[i] = 1;
+            return buffer;
+        }
+    }
     
     return buffer;
 } 
 
 void put_mes(uint16_t* buf_pointer, cab* cab_id){
-
+    cab_id->head = buf_pointer;
+    int i = buf_pointer - cab_id->buffers;
+    cab_id->flags[i] = 0;
+    printf("%d", i);
 }
 
 uint16_t* get_mes(cab* cab_id){
